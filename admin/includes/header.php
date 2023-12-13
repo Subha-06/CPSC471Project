5f5321b03a875351c694c86710dac69aabcf8cdd
Subha <fabihafairuzz@gmail.com>
2023-12-12 18:34:24 -0700

    Admin login page done

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,14 +1,72 @@
-<div class="brand clearfix">
-	<a href="dashboard.php" style="font-size: 20px;">Car Rental Portal | Admin Panel</a>  
-		<span class="menu-btn"><i class="fa fa-bars"></i></span>
-		<ul class="ts-profile-nav">
-			
-			<li class="ts-account">
-				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
-				<ul>
-					<li><a href="change-password.php">Change Password</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
-			</li>
-		</ul>
-	</div>
+<style>
+  header {
+    background-color: #fff;
+    padding: 15px;
+    font-family: Arial, sans-serif;
+  }
+
+  .custom-navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .logo img {
+    max-height: 50px; /* Adjust as needed */
+  }
+
+  .navbar {
+    display: flex;
+    margin-left: 20px; /* Adjust the margin as needed */
+  }
+
+  .navbar ul {
+    list-style: none;
+    display: inline-flex;
+    margin: 20px;
+  }
+
+  .navbar li {
+    margin-right: 100px; /* Adjust margin */
+  }
+
+  .navbar a {
+    text-decoration: none;
+    color: #333;
+  }
+
+  .dropdown {
+    margin-left: 15px;
+    position: relative;
+  }
+
+  .dropdown-menu {
+    position: absolute;
+    top: 80%; /* Adjust positioning */
+    left: 0;
+    list-style: none;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+</style>
+
+<header>
+  <div class="custom-navbar">
+    <!-- Use the same logo as the user navbar -->
+    <div class="logo">
+      <a href="index.php"><img src="img/logo.png" alt="image"/></a>
+    </div>
+    <div class="navbar">
+      <ul class="ts-profile-nav">
+        <li class="ts-account">
+          <ul>
+		  	<li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="change-password.php">Change Password</a></li>
+            <li><a href="logout.php">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</header>
--- admin/index.php
+++ admin/index.php
@@ -4,89 +4,126 @@ include('includes/config.php');
 
 if(isset($_POST['login']))
 {
-$email=$_POST['username'];
-$password=($_POST['password']);
-$sql ="SELECT UserName,Password FROM admin WHERE UserName=:email and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-$_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location = 'change-password.php'; </script>";
-} else{
-
-  echo "<script>alert('Invalid Details');</script>";
-
+    $email=$_POST['username'];
+    $password=($_POST['password']);
+    $sql ="SELECT UserName,Password FROM admin WHERE UserName=:email and Password=:password";
+    $query= $dbh -> prepare($sql);
+    $query-> bindParam(':email', $email, PDO::PARAM_STR);
+    $query-> bindParam(':password', $password, PDO::PARAM_STR);
+    $query-> execute();
+    $results=$query->fetchAll(PDO::FETCH_OBJ);
+    if($query->rowCount() > 0)
+    {
+        $_SESSION['alogin']=$_POST['username'];
+        echo "<script type='text/javascript'> document.location = 'change-password.php'; </script>";
+    } else{
+        echo "<script>alert('Invalid Details');</script>";
+    }
 }
-
-}
-
 ?>
+
 <!doctype html>
 <html lang="en" class="no-js">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<title>Car Rental Portal | Admin Login</title>
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap-social.css">
-	<link rel="stylesheet" href="css/bootstrap-select.css">
-	<link rel="stylesheet" href="css/fileinput.min.css">
-	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-	<link rel="stylesheet" href="css/style.css">
+    <title>Car Rental Portal | Admin Login</title>
+    <style>
+        body {
+            background-color: #f4f4f4;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        html, body {
+            height: 100%;
+        }
+        .form-content {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 70vh;
+            margin: 0;
+        }
+        .well {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 30px;
+        }
+        h1 {
+            color: #333;
+            font-size: 28px;
+            font-weight: bold;
+            margin-bottom: 30px;
+            text-align: center;
+        }
+        label {
+            display: block;
+            margin-bottom: 10px;
+            color: #555;
+            font-size: 14px;
+            font-weight: bold;
+        }
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            font-size: 14px;
+        }
+        button {
+            background-color: #007bff;
+            color: #fff;
+            padding: 12px;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 
 <body>
 
-	<div class="login-page bk-img" style="background-image: url(img/adminlogin.jpg);">
-		<div class="form-content">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3">
-						<h1 class="text-center text-bold text-light mt-4x">Sign in</h1>
-						<div class="well row pt-2x pb-3x bk-light">
-							<div class="col-md-8 col-md-offset-2">
-								<form method="post">
+<!--Header-->
+<?php include('includes/header.php'); ?>
+<!-- /Header -->
 
-									<label for="" class="text-uppercase text-sm">Your Username </label>
-									<input type="text" placeholder="Username" name="username" class="form-control mb">
+<div class="form-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h1 class="text-center text-bold text-light mt-4x">Sign in</h1>
+                <div class="well row pt-2x pb-3x bk-light">
+                    <div class="col-md-8 col-md-offset-2">
+                        <form method="post">
 
-									<label for="" class="text-uppercase text-sm">Password</label>
-									<input type="password" placeholder="Password" name="password" class="form-control mb">
+                            <label for="" class="text-uppercase text-sm">Your Username </label>
+                            <input type="text" placeholder="Username" name="username" class="form-control mb">
 
+                            <label for="" class="text-uppercase text-sm">Password</label>
+                            <input type="password" placeholder="Password" name="password" class="form-control mb">
 
+                            <button class="btn btn-primary btn-block" name="login" type="submit" style="background-color: red;"> LOGIN</button>
 
-									<button class="btn btn-primary btn-block" name="login" type="submit">LOGIN</button>
-
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Loading Scripts -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap-select.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-	<script src="js/dataTables.bootstrap.min.js"></script>
-	<script src="js/Chart.min.js"></script>
-	<script src="js/fileinput.js"></script>
-	<script src="js/chartData.js"></script>
-	<script src="js/main.js"></script>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 

9b35f0c39de70088bfce9ea67e8205c8ffa19097
minhazzz <minhazur.work@gmail.com>
2023-12-12 23:18:41 -0700

    Removing unnecessary shit

--- sqlfile/carrental.sql
+++ sqlfile/carrental.sql
@@ -1,5 +1,3 @@
-
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -14,9 +12,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 
 INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
-(1, 'admin', 'password', '2023-12-12 0:0:0');
-
-
+(1, 'admin', 'password', '2023-01-15 08:30:00');
 
 
 CREATE TABLE IF NOT EXISTS `tblbooking` (
@@ -31,11 +27,8 @@ CREATE TABLE IF NOT EXISTS `tblbooking` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 INSERT INTO `tblbooking` (`id`, `userEmail`, `VehicleId`, `FromDate`, `ToDate`, `message`, `Status`, `PostingDate`) VALUES
-(1, 'rahul.sharma@example.com', 1, '01/01/2017', '04/01/2017', 'Looking forward to this trip, please ensure the vehicle is in good condition.', 1, '2017-04-09 04:47:07'),
-(2, 'priya.gupta@example.com', 2, '11/01/2017', '14/01/2017', 'Excited for the journey, please confirm the booking at the earliest.', 0, '2017-05-29 15:14:28'),
-(3, 'amit.singh@example.com', 3, '21/01/2017', '24/01/2017', 'Requesting early check-in for the vehicle, thank you in advance.', 2, '2017-10-26 12:28:00');
-
-
+(1, 'rahul.sharma@example.com', 1, '2023-02-18', '2023-02-21', 'Looking forward to this trip, please ensure the vehicle is in good condition.', 1, '2023-04-09 12:00:00'), 
+(2, 'priya.gupta@example.com', 2, '2023-03-07', '2023-03-11', 'Excited for the journey, please confirm the booking at the earliest.', 0, '2023-05-29 09:30:00');
 
 CREATE TABLE IF NOT EXISTS `tblbrands` (
   `id` int(11) NOT NULL,
@@ -45,16 +38,13 @@ CREATE TABLE IF NOT EXISTS `tblbrands` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 
-
 INSERT INTO `tblbrands` (`id`, `BrandName`, `CreationDate`, `UpdationDate`) VALUES
-(1, 'Tesla', '2017-09-04 16:39:07', NULL),
-(2, 'Pajero', '2017-05-23 18:22:43', NULL),
-(3, 'Porsche', '2017-10-01 01:47:50', NULL),
-(4, 'Nissan', '2017-04-13 09:47:24', NULL),
-(5, 'Toyota', '2017-05-31 02:36:22', NULL),
-(6, 'Ferrari', '2017-10-07 08:05:48', NULL);
-
-
+(1, 'Tesla', '2023-01-10 14:00:00', NULL), 
+(2, 'Pajero', '2023-02-20 10:30:00', NULL),
+(3, 'Porsche', '2023-03-05 21:15:00', NULL), 
+(4, 'Nissan', '2023-04-02 08:45:00', NULL), 
+(5, 'Toyota', '2023-05-15 17:20:00', NULL), 
+(6, 'Ferrari', '2023-06-30 12:10:00', NULL); 
 
 
 CREATE TABLE IF NOT EXISTS `tblcontactusinfo` (
@@ -65,9 +55,8 @@ CREATE TABLE IF NOT EXISTS `tblcontactusinfo` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 
-
 INSERT INTO `tblcontactusinfo` (`id`, `Address`, `EmailId`, `ContactNo`) VALUES
-(1, 'Test Demo test demo																									', 'test@test.com', '8585233222');
+(1, '1919 J Uni Drive', 'test@test.com', '8585233222');
 
 
 
@@ -84,9 +73,7 @@ CREATE TABLE IF NOT EXISTS `tblcontactusquery` (
 
 
 INSERT INTO `tblcontactusquery` (`id`, `name`, `EmailId`, `ContactNumber`, `Message`, `PostingDate`, `status`) VALUES
-(1, 'Amit Verma', 'amit.verma@example.com', '9876543210', 'I am interested in knowing more about your car rental services. Please provide more details.', '2017-10-16 05:39:33', 1);
-
-
+(1, 'Amit Verma', 'amit.verma@example.com', '9876543210', 'I am interested in knowing more about your car rental services. Please provide more details.', '2023-10-16 05:39:33', 1);
 
 CREATE TABLE IF NOT EXISTS `tblpages` (
   `id` int(11) NOT NULL,
@@ -96,42 +83,14 @@ CREATE TABLE IF NOT EXISTS `tblpages` (
 ) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
 
-
 INSERT INTO `tblpages` (`id`, `PageName`, `type`, `detail`) VALUES
-(1, 'Service Terms', 'service', '<p>Our Service Terms are designed to ensure the best experience for our users.</p>'),
-(2, 'User Privacy Policy', 'privacy', '<p>We are committed to protecting the privacy and security of our users.</p>'),
-(3, 'Our Mission', 'mission', '<p>Learn about our mission, values, and how we strive for excellence.</p>'),
-(4, 'Frequently Asked Questions', 'faqs', '<p>Have questions? Find answers to frequently asked questions here.</p>');
+(1, 'About Us ', 'aboutus', '<span style="color: rgb(0, 0, 0); font-family: "Open Sans", Arial, sans-serif; text-align: justify;">At Pathao our vision is elevating Bangladesh by empowering entrepreneurs through our comprehensive platform.
 
+At Pathao, we prioritize our customers, embody unity in our mission, value every member of our team, and practice empathy and kindness.
 
+We launched in 2015, Pathao is a rapidly growing tech startup offering ample opportunities for personal and professional growth. We encourage our employees to lead initiatives and make a mark.
 
-CREATE TABLE IF NOT EXISTS `tblsubscribers` (
-  `id` int(11) NOT NULL,
-  `SubscriberEmail` varchar(120) DEFAULT NULL,
-  `PostingDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
-
-
-INSERT INTO `tblsubscribers` (`id`, `SubscriberEmail`, `PostingDate`) VALUES
-(1, 'rahul.sharma@example.com', '2017-11-07 22:05:40');
-
-
-
-CREATE TABLE IF NOT EXISTS `tbltestimonial` (
-  `id` int(11) NOT NULL,
-  `UserEmail` varchar(100) NOT NULL,
-  `Testimonial` mediumtext NOT NULL,
-  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
-
-INSERT INTO `tbltestimonial` (`id`, `UserEmail`, `Testimonial`, `PostingDate`, `status`) VALUES
-(1, 'aditya.kumar@example.com', 'Outstanding experience with the rental service. The car was clean and well-maintained.', '2017-11-12 03:16:17', 1),
-(2, 'priya.singh@example.com', 'Impressed by the quick service and the quality of the vehicle. Highly recommend.', '2017-02-23 21:51:24', 1);
-
-
+We\'re committed to digitizing Bangladesh with an all-in-one app solution for ride-sharing, food delivery, and e-commerce logistics, leveraging technology to simplify lives.</span>');
 
 CREATE TABLE IF NOT EXISTS `tblusers` (
   `id` int(11) NOT NULL,
@@ -150,12 +109,8 @@ CREATE TABLE IF NOT EXISTS `tblusers` (
 
 
 INSERT INTO `tblusers` (`id`, `FullName`, `EmailId`, `Password`, `ContactNo`, `dob`, `Address`, `City`, `Country`, `RegDate`, `UpdationDate`) VALUES
-(1, 'Amit Sharma', 'demo@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2147483647', NULL, NULL, NULL, NULL, '2017-06-17 19:59:27', '2017-06-26 21:02:58'),
-(2, 'Raj Singh', 'anuj@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '8285703354', NULL, NULL, NULL, NULL, '2017-06-17 20:00:49', '2017-06-26 21:03:09'),
-(3, 'Anjali Kumar', 'webhostingamigo@gmail.com', 'f09df7868d52e12bba658982dbd79821', '09999857868', '03/02/1990', 'PKL', 'PKL', 'PKL', '2017-06-17 20:01:43', '2017-06-17 21:07:41'),
-(4, 'Vikram Reddy', 'test@gmail.com', '5c428d8875d2948607f3e3fe134d71b4', '9999857868', '', 'PKL', 'XYZ', 'XYZ', '2017-06-17 20:03:36', '2017-06-26 19:18:14');
-
-
+(1, 'Minhazur Rahman', 'minhaz@gmail.com', '12345', '4039289019', NULL, NULL, NULL, NULL, '2023-06-17 19:59:27', '2023-06-26 21:02:58'),
+(2, 'Subha M', 'subha@gmail.com', '1234', '4039878914', NULL, NULL, NULL, NULL, '2023-06-17 20:00:49', '2023-06-26 21:03:09');
 
 CREATE TABLE IF NOT EXISTS `tblvehicles` (
   `id` int(11) NOT NULL,
@@ -166,11 +121,7 @@ CREATE TABLE IF NOT EXISTS `tblvehicles` (
   `FuelType` varchar(100) DEFAULT NULL,
   `ModelYear` int(6) DEFAULT NULL,
   `SeatingCapacity` int(11) DEFAULT NULL,
-  `Vimage1` varchar(120) DEFAULT NULL,
-  `Vimage2` varchar(120) DEFAULT NULL,
-  `Vimage3` varchar(120) DEFAULT NULL,
-  `Vimage4` varchar(120) DEFAULT NULL,
-  `Vimage5` varchar(120) DEFAULT NULL,
+  `Vimage` varchar(120) DEFAULT NULL,
   `AirConditioner` int(11) DEFAULT NULL,
   `PowerDoorLocks` int(11) DEFAULT NULL,
   `AntiLockBrakingSystem` int(11) DEFAULT NULL,
@@ -189,12 +140,12 @@ CREATE TABLE IF NOT EXISTS `tblvehicles` (
 
 
 
-INSERT INTO `tblvehicles` (`id`, `VehiclesTitle`, `VehiclesBrand`, `VehiclesOverview`, `PricePerDay`, `FuelType`, `ModelYear`, `SeatingCapacity`, `Vimage1`, `Vimage2`, `Vimage3`, `Vimage4`, `Vimage5`, `AirConditioner`, `PowerDoorLocks`, `AntiLockBrakingSystem`, `BrakeAssist`, `PowerSteering`, `DriverAirbag`, `PassengerAirbag`, `PowerWindows`, `CDPlayer`, `CentralLocking`, `CrashSensor`, `LeatherSeats`, `RegDate`, `UpdationDate`) VALUES
-(1, 'Swift Desire', 1, 'Compact and efficient, perfect for city driving.', 1200, 'Petrol', 2019, 5, 'image1.jpg', 'image1.jpg', 'image1.jpg', 'image1.jpg', 'image1.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2017-08-19 19:04:17', '2017-02-06 14:59:19'),
-(2, 'Hyundai Creta', 3, 'A spacious SUV with a stylish design.', 1800, 'Diesel', 2020, 7, 'image2.jpg', 'image2.jpg', 'image2.jpg', 'image2.jpg', 'image2.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2017-10-19 15:56:09', '2017-11-20 08:18:05'),
-(3, 'Mahindra Thar', 4, 'Rugged and adventurous off-road vehicle.', 1500, 'Diesel', 2018, 4, 'image3.jpg', 'image3.jpg', 'image3.jpg', 'image3.jpg', 'image3.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2017-08-19 17:42:00', '2017-10-25 22:46:33'),
-(4, 'Ford EcoSport', 2, 'Stylish, safe, and comfortable for the family.', 1600, 'Petrol', 2017, 5, 'image4.jpg', 'image4.jpg', 'image4.jpg', 'image4.jpg', 'image4.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2017-12-06 13:49:43', '2017-09-21 20:32:08'),
-(5, 'Honda City', 5, 'Sleek sedan with advanced features.', 1400, 'Petrol', 2021, 5, 'image5.jpg', 'image5.jpg', 'image5.jpg', 'image5.jpg', 'image5.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2017-03-22 09:46:37', '2017-01-29 23:50:07');
+INSERT INTO `tblvehicles` (`id`, `VehiclesTitle`, `VehiclesBrand`, `VehiclesOverview`, `PricePerDay`, `FuelType`, `ModelYear`, `SeatingCapacity`, `Vimage`, `AirConditioner`, `PowerDoorLocks`, `AntiLockBrakingSystem`, `BrakeAssist`, `PowerSteering`, `DriverAirbag`, `PassengerAirbag`, `PowerWindows`, `CDPlayer`, `CentralLocking`, `CrashSensor`, `LeatherSeats`, `RegDate`, `UpdationDate`) VALUES
+(1, 'Swift Desire', 1, 'Compact and efficient, perfect for city driving.', 1200, 'Petrol', 2019, 5, 'image1.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2023-08-19 19:04:17', '2023-02-06 14:59:19'),
+(2, 'Hyundai Creta', 3, 'A spacious SUV with a stylish design.', 1800, 'Diesel', 2020, 7, 'image2.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2023-10-19 15:56:09', '2023-11-20 08:18:05'),
+(3, 'Mahindra Thar', 4, 'Rugged and adventurous off-road vehicle.', 1500, 'Diesel', 2018, 4, 'image3.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2023-08-19 17:42:00', '2023-10-25 22:46:33'),
+(4, 'Ford EcoSport', 2, 'Stylish, safe, and comfortable for the family.', 1600, 'Petrol', 2023, 5, 'image4.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2023-12-06 13:49:43', '2023-09-21 20:32:08'),
+(5, 'Honda City', 5, 'Sleek sedan with advanced features.', 1400, 'Petrol', 2021, 5, 'image5.jpg', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, '2023-03-22 09:46:37', '2023-01-29 23:50:07');
 
 
 ALTER TABLE `admin`
@@ -218,14 +169,6 @@ ALTER TABLE `tblcontactusquery`
 
 
 ALTER TABLE `tblpages`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `tblsubscribers`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `tbltestimonial`
   ADD PRIMARY KEY (`id`);
 
 
@@ -253,13 +196,8 @@ ALTER TABLE `tblcontactusquery`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 
 ALTER TABLE `tblpages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 
-ALTER TABLE `tblsubscribers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
-
-ALTER TABLE `tbltestimonial`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 
 ALTER TABLE `tblusers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
